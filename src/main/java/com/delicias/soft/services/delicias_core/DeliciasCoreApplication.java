c9d7b790ce6692115e86c751efa3db5d1f7e13b2
jdev <juanj@jdev.com>
2025-09-30 10:41:55 -0600

    add initial project

---
+++ src/main/java/com/delicias/soft/services/delicias_core/DeliciasCoreApplication.java
@@ -0,0 +1,13 @@
+package com.delicias.soft.services.delicias_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciasCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciasCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delicias/soft/services/delicias_core/DeliciasCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delicias.soft.services.delicias_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciasCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
